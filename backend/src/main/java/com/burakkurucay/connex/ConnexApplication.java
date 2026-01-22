7da4241f37b9d670b0bbadaa2661a573f62979e4
214X <92593070+214X@users.noreply.github.com>
2026-01-22 13:59:03 +0300

    Spring installation completed.

---
+++ backend/src/main/java/com/burakkurucay/connex/ConnexApplication.java
@@ -0,0 +1,13 @@
+package com.burakkurucay.connex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnexApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/burakkurucay/connex/ConnexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakkurucay.connex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
